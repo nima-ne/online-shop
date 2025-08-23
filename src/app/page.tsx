88036dd89ffbca7001813ebb97f7a4cc386f1494
unknown <nimaneshat44@gmail.com>
2025-08-23 23:19:14 +0430

    Fix page.tsx for App Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,22 @@ export interface ProType {
   id: number;
   image: string;
   price: number;
-  rating: { rate: number ,  count: number };
+  rating: { rate: number; count: number };
   title: string;
 }
 
-async function Home() {
-  const callProducts = await axios.get("https://fakestoreapi.com/products");
-  const products = callProducts.data;
 
-  console.log(products);
+export default async function Page() {
+  const callProducts = await axios.get("https://fakestoreapi.com/products");
+  const products = callProducts.data as ProType[];
 
   return (
     <Container>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-        {products.map((product: ProType) => (
-          <ProductCard product={product} key={product.id}/>
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
         ))}
       </div>
     </Container>
   );
 }
-
-export default Home;
